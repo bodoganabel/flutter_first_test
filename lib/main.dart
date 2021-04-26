bf6a1fdd6e6246a38b71a0f8f634d79de2c9bc2c
bodoganabel <abel.bodogan@zooshgroup.com>
2021-04-26 15:58:23 +0200

    Image-from-network

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Image.asset('assets/space-1.jpg'),
+        child: Image.network('https://images.unsplash.com/photo-1484589065579-248aad0d8b13?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=696&q=80'),
       ),
       floatingActionButton: FloatingActionButton(
         child: Text("Click"),
